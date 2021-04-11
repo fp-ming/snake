7a4b73f614fbcba43e36a5634de2bacee9c1278e
fp-ming <67403688+fp-ming@users.noreply.github.com>
2021-04-11 19:45:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const snakeFood = document.getElementById('snake_food');
 const container = document.getElementById('container');
 changePos (snakeFood,container);
 
+// 点击事件
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+const up = document.getElementById('up');
+const down = document.getElementById('down');
+left.addEventListener('click', eventHandle);
+right.addEventListener('click', eventHandle);
+up.addEventListener('click', eventHandle);
+down.addEventListener('click', eventHandle);
+
 // 控制按钮，防止过快的操作
 let press_disable = false;
 
@@ -18,36 +28,4 @@ let t = null;
 timeout(1000);
 
 // 控制蛇头移动方向的事件，不能反方向
-document.onkeyup = function eventHandle (event) {
-	if (press_disable) return;
-	const e = event || window.event;
-	const dir = snake.direction;
-	press_disable = true;
-	switch (e.keyCode) {
-		// 向左
-		case 37: 
-			if(dir == 'right') return;
-			snake.changeDirection('left');break;
-
-		// 向上
-		case 38: 
-			if(dir == 'down') return;
-			snake.changeDirection('up');break;
-
-		// 向右
-		case 39: 
-			if(dir == 'left') return;
-			snake.changeDirection('right');break;
-		
-		// 向下
-		case 40: 
-			if(dir == 'up') return;
-			snake.changeDirection('down');break;
-
-		// 暂停
-		case 80: pause();break;
-
-		// 结束游戏
-		case 81: gameOver();break;
-	}
-}
+document.onkeyup = eventHandle;

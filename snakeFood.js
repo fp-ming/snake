7df79495a5e832e79d517e192e0c1c7c68dc73a0
fp-ming <67403688+fp-ming@users.noreply.github.com>
2021-04-11 10:58:16 +0800

    Update snakeFood.js

--- snakeFood.js
+++ snakeFood.js
@@ -1,26 +1,10 @@
 /**
 *	此文件用以初始化和实时改变蛇食的位置
 */
-const snakeFood = document.getElementById('snake_food');
-const screen = document.getElementById('screen');
-
-// 获取元素属性
-function elAttr (el) {
-	const element = {
-		width: 	el.clientWidth, 
-		height: el.clientHeight,
-		posX: 	el.offsetLeft,
-		posY: 	el.offsetTop
-	}
-
-	return element;
-}
 
 function changePos (snakeFood,screen) {
-	let _x = Math.ceil(Math.random()*(elAttr(screen).width - elAttr(snakeFood).width));
-	let _y = Math.ceil(Math.random()*(elAttr(screen).height - elAttr(snakeFood).height));
+	let _x = Math.ceil(Math.random()*(elAttr(screen).width - elAttr(snakeFood).width) - 5) + 5;
+	let _y = Math.ceil(Math.random()*(elAttr(screen).height - elAttr(snakeFood).height) - 5)+ 5;
 	snakeFood.style.left = _x + 'px';
 	snakeFood.style.top = _y + 'px';
 }
-
-changePos (snakeFood,screen)

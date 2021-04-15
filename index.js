dd330a892b7fa9d8f00878e8f2438ed76b8ca7e3
fp-ming <67403688+fp-ming@users.noreply.github.com>
2021-04-15 23:02:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const container = document.getElementById('container');
 changePos (snakeFood,container);
 
 // 获得元素节点
-const left = document.getElementById('left');
+const left  = document.getElementById('left');
 const right = document.getElementById('right');
-const up = document.getElementById('up');
-const down = document.getElementById('down');
-const fast = document.getElementById('fast');
+const up    = document.getElementById('up');
+const down  = document.getElementById('down');
+const fast  = document.getElementById('fast');
 // 绑定事件，兼容IE
 if(document.addEventListener) {
 	left.addEventListener('click', eventHandle);
 	right.addEventListener('click', eventHandle);
 	up.addEventListener('click', eventHandle);
 	down.addEventListener('click', eventHandle);
-	fast.addEventLinster('mousedown',fastEvent);
+	fast.addEventListener('mousedown',fastEvent);
 } else {
 	left.attachEvent('onclick', eventHandle);
 	right.attachEvent('onclick', eventHandle);

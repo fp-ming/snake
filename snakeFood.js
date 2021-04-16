0aef0fd5b882106ff2d1f1e4b58530c472a3a847
fp-ming <67403688+fp-ming@users.noreply.github.com>
2021-04-16 21:14:49 +0800

    Update snakeFood.js

--- snakeFood.js
+++ snakeFood.js
@@ -7,5 +7,7 @@ function changePos (snakeFood,screen) {
 	let _y = Math.ceil(Math.random()*(elAttr(screen).height - elAttr(snakeFood).height) - 5)+ 5;
 	snakeFood.style.left = _x + 'px';
 	snakeFood.style.top = _y + 'px';
-	
+	if(snake.bodys.some(el => collision(el,snakeFood)) {
+		changePos (snakeFood,screen);
+	}
 }
